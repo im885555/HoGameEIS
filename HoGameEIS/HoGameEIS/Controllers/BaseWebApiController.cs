56abe205dae9d092da88b1e6de4c41306c3157e4
Gene <im885555@gmail.com>
2015-07-02 10:56:06 +0800

    a

---
+++ HoGameEIS/HoGameEIS/Controllers/BaseWebApiController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HoGameEIS.Controllers
+{
+    public class BaseWebApiController : ApiController
+    {
+        public BaseWebApiController() 
+        {
+            if (!User.Identity.IsAuthenticated)
+            {
+                var response = new HttpResponseMessage();
+                response.StatusCode = (HttpStatusCode)401;
+                response.ReasonPhrase = "You are not authorized";
+
+                throw new HttpResponseException(response);
+            }    
+        }
+    }
+}
---
+++ HoGameEIS/HoGameEIS/Controllers/GroupBuyApiController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HoGameEIS.Controllers
+{
+    public class GroupBuyApiController : BaseWebApiController
+    {
+        public GroupBuyApiController() 
+        {
+        }
+
+        // GET api/groupbuyapi
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/groupbuyapi/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/groupbuyapi
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/groupbuyapi/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/groupbuyapi/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
