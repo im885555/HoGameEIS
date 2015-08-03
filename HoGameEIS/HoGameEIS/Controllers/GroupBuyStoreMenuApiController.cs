21cd79a16633af527190cd120589e6043cc97ae9
Gene <im885555@gmail.com>
2015-08-03 13:52:22 +0800

    a

---
+++ HoGameEIS/HoGameEIS/Controllers/GroupBuyStoreMenuApiController.cs
@@ -0,0 +1,41 @@
+﻿using HoGameEIS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HoGameEIS.Controllers
+{
+    public class GroupBuyStoreMenuApiController : ApiController
+    {
+        // GET: api/GroupBuyStoreMenuApi
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/GroupBuyStoreMenuApi/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/GroupBuyStoreMenuApi
+        public void Post([FromBody]GroupBuyStoreItem value)
+        {
+           
+        }
+
+        // PUT: api/GroupBuyStoreMenuApi/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/GroupBuyStoreMenuApi/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
