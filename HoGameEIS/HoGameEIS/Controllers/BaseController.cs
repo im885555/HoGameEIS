aea23f9e832b4f0ef8487013e4a6019d7a76afad
Gene <im885555@gmail.com>
2015-08-27 18:05:25 +0800

    a

---
+++ HoGameEIS/HoGameEIS/Controllers/BaseController.cs
@@ -0,0 +1,34 @@
+﻿using HoGameEIS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace HoGameEIS.Controllers
+{
+    public class BaseController : Controller
+    {
+        protected Employee CurrentUser()
+        {
+            Employee emp = new Employee();
+            if (User != null && User.Identity.IsAuthenticated)
+            {
+                FormsIdentity id = (FormsIdentity)User.Identity;
+                // 再取出使用者的 FormsAuthenticationTicket
+                FormsAuthenticationTicket ticket = id.Ticket;
+
+                System.Web.Script.Serialization.JavaScriptSerializer objSerializer = new System.Web.Script.Serialization.JavaScriptSerializer();
+
+                emp = objSerializer.Deserialize<Employee>(ticket.UserData);
+            }
+            return emp;
+        }
+
+        public BaseController()
+        {
+            
+        }
+    }
+}
