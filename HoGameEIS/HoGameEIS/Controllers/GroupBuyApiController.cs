5e08b0c2f6bf43142d10c0a6edc2ed55e3b9f852
Gene <im885555@gmail.com>
2015-08-27 17:56:09 +0800

    order

--- HoGameEIS/HoGameEIS/Controllers/GroupBuyApiController.cs
+++ HoGameEIS/HoGameEIS/Controllers/GroupBuyApiController.cs
@@ -6,8 +6,13 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 using System.Web;
 using System.Web.Http;
+using System.Web.WebSockets;
 
 namespace HoGameEIS.Controllers
 {
@@ -153,27 +158,60 @@ namespace HoGameEIS.Controllers
         }
 
         //[Route("api/GroupbuyWS")]
+        //[HttpGet]
         //public HttpResponseMessage GroupbuyWS(GroupBuy groupBuy)
         //{
-        //    HttpContext.Current.AcceptWebSocketRequest(new ChatWebSocketHandler("aaa"));
-        //    return Request.CreateResponse(HttpStatusCode.SwitchingProtocols);
+        //    //HttpContext.Current.AcceptWebSocketRequest(new ChatWebSocketHandler("aaa"));
+        //    //return Request.CreateResponse(HttpStatusCode.SwitchingProtocols);
+        //    if (HttpContext.Current.IsWebSocketRequest)
+        //    {
+        //        HttpContext.Current.AcceptWebSocketRequest(ProcessWSChat);
+        //    }
+        //    return new HttpResponseMessage(HttpStatusCode.SwitchingProtocols);
+
+        //}
+
+        //private async Task ProcessWSChat(AspNetWebSocketContext context)
+        //{
+        //    WebSocket socket = context.WebSocket;
+        //    while (true)
+        //    {
+        //        ArraySegment<byte> buffer = new ArraySegment<byte>(new byte[1024]);
+        //        WebSocketReceiveResult result = await socket.ReceiveAsync(
+        //            buffer, CancellationToken.None);
+        //        if (socket.State == WebSocketState.Open)
+        //        {
+        //            string userMessage = Encoding.UTF8.GetString(
+        //                buffer.Array, 0, result.Count);
+        //            userMessage = "You sent: " + userMessage + " at " +
+        //                DateTime.Now.ToLongTimeString();
+        //            buffer = new ArraySegment<byte>(
+        //                Encoding.UTF8.GetBytes(userMessage));
+        //            await socket.SendAsync(
+        //                buffer, WebSocketMessageType.Text, true, CancellationToken.None);
+        //        }
+        //        else
+        //        {
+        //            break;
+        //        }
+        //    }
         //}
 
         //class ChatWebSocketHandler : WebSocketHandler
         //{
         //    private static WebSocketCollection _chatClients = new WebSocketCollection();
         //    private string _username;
-      
+
         //    public ChatWebSocketHandler(string username)
-        //        {
+        //    {
         //        _username = username;
-        //        }
-      
+        //    }
+
         //    public override void OnOpen()
-        //        {
+        //    {
         //        _chatClients.Add(this);
         //    }
-     
+
         //    public override void OnMessage(string message)
         //    {
         //        _chatClients.Broadcast(_username + ": " + message);
